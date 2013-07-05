1bc22d3f8893f651af7560086d3025d28a941f30
EaseWay Hu <easeway@gmail.com>
2013-07-05 17:05:23 +0800

    refactor with term-dashboard

--- incubate.js
+++ incubate.js
@@ -1,101 +1,221 @@
-var os       = require('os'),
-    path     = require('path'),
-    async    = require('async'),
-    Class    = require('js-class'),
-    elements = require('evo-elements'),
-    conf     = elements.Config.conf(),
-    
-    Packages    = require('./lib/Packages'),
-    FileCache   = require('./lib/FileCache'),
-    BuildEngine = require('./lib/BuildEngine');
-    
-var App = Class({
-    constructor: function () {
-        this.cfg = {
-            pkgPaths: conf.query('package.path', ['./packages']),
-            cacheDir: path.resolve(conf.query('package.cache', path.join('_build', 'cache'))),
-            buildDir: path.resolve(conf.query('build.dir', '_build')),
-            parallel: parseInt(conf.query('parallel')),
-            parallelMax: parseInt(conf.query('parallelMax'))
-        };
-        Array.isArray(this.cfg.pkgPaths) || (this.cfg.pkgPaths = [this.cfg.pkgPaths]);
-        this.cfg.pkgPaths = this.cfg.pkgPaths.map(function (dir) { return path.resolve(dir); });
-        isNaN(this.cfg.parallel) && (this.cfg.parallel = os.cpus().length);
-        isNaN(this.cfg.parallelMax) && (this.cfg.parallelMax = 4);
-        this.cfg.parallel > this.cfg.parallelMax && (this.cfg.parallel = this.cfg.parallelMax);
-        
-        this.packages = new Packages(this.cfg.pkgPaths);
-        this.cache = new FileCache(this.cfg.cacheDir, this.cfg.parallel);
-    },
-    
-    run: function () {
-        try {
-            this.packages.load(conf.args);
-        } catch (err) {
-            console.error(err.message);
-            process.exit(1);
-        }
+var conf      = require('evo-elements').Config.conf(),
+    Dashboard = require('term-dashboard').Dashboard,
+    bytes     = require('bytes'),
+    clicolor  = require('cli-color'),
+    Builder   = require('./index').Builder;
 
-        async.series([
-            this.download.bind(this),
-            this.build.bind(this)
-        ], function (err) {
-            err && console.error(err.message);
-            err && process.exit(1);
-            process.exit(0);
-        });
-    },
-    
-    download: function (callback) {
-        var completed = 0, total = 0;
-        var complete = function (err) {
-            this.cache.removeAllListeners();
-            console.log('');
-            callback(err);
-        }.bind(this);
-        
-        this.cache.on('complete', function (file, err) {
-            err && complete(err);
-            err || (++ completed && completed >= total && complete());
-        }).message('Downloading packages ...');
-        
-        var order = this.packages.order();
-        async.whilst(
-            function () { return !order.empty; },
-            function (next) {
-                var pkgs = order.fetch('all');
-                total += pkgs.length;
-                pkgs.forEach(function (pkg) {
-                    pkg.sources.forEach(function (src) {
-                        this.cache.enqueue(src);
-                    }.bind(this));
-                }.bind(this));
-                order.complete(pkgs);
-                next();
-            }.bind(this),
-            function () { completed >= total && complete(); }
-        );
-    },
-    
-    build: function (callback) {
-        var order = this.packages.order();
-        async.whilst(
-            function () { return !order.empty; },
-            function (next) {
-                var pkgs = order.fetch(this.cfg.parallel);
-                async.each(pkgs, this._buildPkg.bind(this), function (err) {
-                    pkgs.forEach(function (pkg) { order.complete(pkg); });
-                    next(err);
-                });
-            }.bind(this),
-            callback
-        );
-    },
-    
-    _buildPkg: function (pkg, done) {
-        console.log('BUILD ' + pkg.fullName);        
-        BuildEngine.build(pkg, this.cache.basedir(pkg), this.cfg.buildDir, done);
+var cfg = {
+    pkgPaths: conf.query('package.path'),
+    buildDir: conf.query('build.dir', '_build'),
+    cacheDir: conf.query('package.cache'),
+    parallel: conf.query('parallel'),
+    parallelMax: conf.query('parallelMax')
+};
+
+if (!cfg.pkgPaths) {
+    if (process.env.PACKAGE_PATH) {
+        cfg.pkgPaths = process.env.PACKAGE_PATH.split(':');
+    } else {
+        console.error('No package path found, specify via --package-path or set PACKAGE_PATH.');
+        process.exit(1);
     }
+}
+
+function colWorkerId() {
+    return { text: function (data) { return data.worker != undefined ? data.worker + ': ' : ''; }, width: 4, align: 'right' };
+}
+
+function colAction(text, fg) {
+    return { text: text, width: 8, fg: (fg || 'gray') };
+}
+
+function colPackage(unlimited, fg) {
+    var view = { key: 'pkgName', width: 20, fg: (fg || 'brightWhite'), clip: 'ellipsis' };
+    if (unlimited) {
+        delete view.width;
+    }
+    return view;
+}
+
+function colFilename(unlimited, fg) {
+    var view = { key: 'filename', fg: 'gray', clip: 'ellipsis' };
+    if (!unlimited) {
+        view.width = 30;
+        view.fg = 'brightWhite';
+    }
+    fg && (view.fg = fg);
+    return view;
+}
+
+function colSteps() {
+    return {
+        text: function (data) {
+            return data.stepIndex + '/' + data.totalSteps;
+        },
+        width: 10,
+        align: 'right'
+    };
+}
+
+var dashboard = new Dashboard({
+    views: {
+        'header': [
+            { key: 'title', width: 15, fg: 'brightWhite', styles: ['underline'] },
+            { key: 'action', fg: 'cyan', styles: ['bold'] }
+        ],
+        'footer': [
+            { key: 'message', fg: 'brightWhite' }
+        ],
+        'idle-worker': [
+            colWorkerId(),
+            { text: 'Idle', fg: 'gray' }
+        ],
+        'package-examine': [
+            colWorkerId(),
+            colAction('EXAMINE'),
+            colPackage(true)
+        ],
+        'package-loading': [
+            colWorkerId(),            
+            colAction('LOADING', 'green'),
+            colPackage(),
+            colFilename(true)
+        ],
+        'package-loaded': [
+            colWorkerId(),            
+            colAction('LOADED', 'brightGreen'),
+            colPackage(),
+            colFilename(true)
+        ],
+        'source-validating': [
+            colWorkerId(),            
+            colAction('CHECK'),
+            colPackage(),
+            colFilename(true)
+        ],
+        'source-validated': [
+            colWorkerId(),            
+            colAction('CHECK'),
+            colPackage(),
+            colFilename(true),
+            { text: function (data) { return data.valid ? { text: 'OK', fg: 'green' } : { text: 'INVALID', fg: 'red' }; }, width: 8, align: 'right' },
+            { text: ' ', width: 8 }
+        ],
+        'source-download': [
+            colWorkerId(),            
+            colAction('FETCH'),
+            colPackage(),
+            colFilename(),
+            { key: 'url', fg: 'white', clip: 'ellipsis' }
+        ],
+        'source-failure': [
+            colWorkerId(),            
+            colAction('FAILURE', 'red'),
+            colPackage(),
+            colFilename(),
+            { key: 'url', fg: 'white', clip: 'ellipsis' }
+        ],
+        'source-progress': [
+            colWorkerId(),            
+            colAction('FETCH', 'green'),
+            colPackage(),
+            colFilename(),
+            { renderer: 'progressbar', key: ['downloaded', 'size'] },
+            { text: function (data) { return Math.floor(data.downloaded * 100 / data.size) + '%'; }, width: 6 },
+            { text: function (data) { return (bytes(data.downloaded) + ' / ' + bytes(data.size)).toUpperCase(); }, width: 20 }
+        ],
+        'build-prepare': [
+            colWorkerId(),            
+            colAction('PREPARE'),
+            colPackage(true)
+        ],
+        'build-step': [
+            colWorkerId(),            
+            colAction('BUILD', 'green'),
+            colPackage(),
+            colSteps()
+        ],
+        'build-step-event': [
+            colWorkerId(),            
+            colAction('BUILD', 'green'),
+            colPackage(),
+            colSteps(),
+            { key: 'command', clip: 'ellipsis' }
+        ],
+        'build-finishing': [
+            colWorkerId(),            
+            colAction('FINISH', 'green'),
+            colPackage(true)
+        ],
+        'build-failed': [
+            colWorkerId(),
+            colAction('FAILED', 'red'),
+            colPackage(),
+            { key: 'message', fg: 'red', clip: 'collapsis' }
+        ]
+    },
+    
+    layout: ['header', 'info', 'footer']
 });
 
-new App().run();
+var translates = {
+    package: function (msg) {
+        msg.info && (msg.name = msg.info.name);
+        msg.info && (msg.ver = msg.info.ver);
+        msg.name && (msg.pkgName = msg.name + (msg.ver ? '-' + msg.ver : ''));
+        return msg;        
+    },
+    
+    source: function (msg) {
+        msg.filename = msg.file.file;
+        msg.pkgName = msg.file.pkg.fullName;
+        return msg;
+    },
+    
+    build: function (msg) {
+        msg.pkgName = msg.pkg.fullName;
+        msg.totalSteps = msg.pkg.buildSteps.length;
+        msg.stepEvent == 'command' && (msg.command = msg.params.command);
+        msg.error && (msg.message = msg.error.message);
+        return msg;
+    }
+};
+
+var startTime = new Date();
+dashboard
+    .update('header', 0, 'header', { title: 'Incubator', action: 'LOADING' })
+    .update('footer', 0, 'footer', { message: 'Incubation started at ' + startTime + ', Ctrl-C to break' });
+
+var builder = new Builder(cfg)
+    .on('packages', function () {
+            dashboard
+                .refresh([
+                    'header',
+                    'empty',
+                    { name: 'worker', rows: builder.cfg.parallel },
+                    'empty',
+                    'footer'
+                ])
+                .update('header', 0, 'header', { title: 'Incubator', action: 'BUILDING' });
+            for (var i = 0; i < builder.cfg.parallel; i ++) {
+                dashboard.update('worker', i, 'idle-worker', { worker: i });
+            }
+        })
+    .on('notify', function (msg) {
+            var view = msg.src + '-' + msg.event;
+            msg.src == 'package' ?
+                dashboard.update('info', 0, view, translates[msg.src](msg)) :
+                dashboard.update('worker', msg.worker, view, translates[msg.src](msg));
+        });
+    
+builder.run(conf.args, function (err) {
+    var endTime = new Date();
+    dashboard
+        .update('header', 0, 'header', { title: 'Incubator', action: 'COMPLETE' })
+        .update('footer', 0, 'footer', {
+            message: 'Incubation ' + (err ? clicolor.red('FAILED') : clicolor.green('SUCCEEDED')) + ' at ' + endTime + ', took ' + ((endTime - startTime) / 1000) + 's'
+        });
+    err && dashboard.term.fg.red().write(err.message).reset().write("\r\n");
+    dashboard.term.write("\r\n");
+    process.exit(err ? 1 : 0);
+});
--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 module.exports = {
-    Package:  require('./lib/Package'),
-    Packages: require('./lib/Packages')
+    Package:     require('./lib/Package'),
+    Packages:    require('./lib/Packages'),
+    Digests:     require('./lib/Digests'),
+    Downloader:  require('./lib/Downloader'),
+    FileCache:   require('./lib/FileCache'),
+    BuildEngine: require('./lib/BuildEngine'),
+    Builder:     require('./lib/Builder')
 };
